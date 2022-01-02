46048538e0f5b0026733b48ea7aa27b6a8d16f26
jmholzer <jannic.holzer@gmail.com>
2022-01-02 04:31:13 +0100

    Remove create_list.py (errant testing script).

--- hash_table/create_list.py
+++
@@ -1,6 +0,0 @@
-from pprint import pprint
-import random, string
-
-str_dict = str_dict = {''.join(random.choices(string.ascii_uppercase + string.digits, k=10)): "test" for _ in range(130)} 
-
-pprint(str_dict)
